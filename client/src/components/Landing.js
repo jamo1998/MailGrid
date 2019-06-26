8641729f4cb66701413b1107fd12fad32a767b15
Jameson Aardalen <jamesonaardalen@gmail.com>
2019-06-25 18:51:24 -0700

    styled landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,47 @@
+import './Landing.css';
 import React from 'react';
 
 const Landing = () => {
   return (
-    //2 curley braces, 1st one indicates were about to pass in
-    //some javaScript, 2nd represents the object we're passing
-    <div style={{ textAlign: 'center' }}>
-      <h1>
-        MailGrid!
-      </h1>
-      Collect feedback from your users
+    <div className="landing center">
+      <h1 className="landing-header">Welcome to MailGrid</h1>
+      <h5>Sending Surveys Has Never Been Easier!</h5>
+
+      <div className="row">
+        <div className="col s12 m4">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <i className="material-icons large">create</i>
+              <span className="card-title">Create Surveys For Your Products</span>
+              <p>Construct survey campaigns that are based on
+                products from your own company!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col s12 m4">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <i className="material-icons large">send</i>
+            <span className="card-title">Mass Email Your Surveys</span>
+            <p>This app uses the sendGrid API to send out mass email campaigns with one click!</p>
+          </div>
+        </div>
+        </div>
+
+        <div className="col s12 m4">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <i className="material-icons large">group</i>
+              <span className="card-title">Track Your Client's Responses</span>
+              <p>All of your responses will be displayed in the user dashboard
+                from every survey campaign you start.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
